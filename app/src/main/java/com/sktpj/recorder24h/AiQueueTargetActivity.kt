76e10843421ecd3f89b2a2fdc50b243e308399e8
skt-pj <134942813+skt-pj@users.noreply.github.com>
2026-08-12 00:47:29 +0900

    Fix target notebook composable import

--- app/src/main/java/com/sktpj/recorder24h/AiQueueTargetActivity.kt
+++ app/src/main/java/com/sktpj/recorder24h/AiQueueTargetActivity.kt
@@ -22,6 +22,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
